678ff86d883a92f8f1efa1266e36d4f728e45c93
Aditya Vasantharao <hsfan612@gmail.com>
2020-11-18 16:16:00 -0500

    Add OpenCV logo projection solution

--- Lecture 1/shell.py
+++ Lecture 1/shell.py
@@ -5,4 +5,12 @@ bg = cv2.imread( "4season.png" )
 fg = cv2.imread( "opencv.png" )
 fgmask = cv2.imread( "opencv_mask.png" )
 
+
 # YOUR CODE HERE. EXPECTED OUTPUT: OPENCV LOGO PLACED ON TOP OF BACKGROUND
+
+def aand(a, b):
+    return cv2.bitwise_and(a, b)
+def oor(a, b):
+    return cv2.bitwise_or(a, b)
+    
+new = cv2.imwrite("final.png", aand(oor(bg, fgmask), fg))
